df0ee93d42882930df71a23c541750a13e871349
DESKTOP-VBATKI1\Owner <voyager.teng@gmail.com>
2022-08-24 19:55:37 +0800

    帳號功能-用MD5加密密碼

--- springboot-mall/src/main/java/com/tom/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/tom/springbootmall/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -33,6 +34,11 @@ public class UserServiceImpl implements UserService {
     public Integer register(UserRegisterRequest userRegisterRequest){
         //在註冊前就要先檢查是否email已被註冊過
         User user=userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        //把前端資訊轉換成hash value
+        String hashedPassword= DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         if (user!=null){
             //當DB中找到已被註冊的帳號時，則噴出一個exception
             log.warn("該email:{}已被註冊",userRegisterRequest.getEmail());
@@ -51,13 +57,21 @@ public class UserServiceImpl implements UserService {
      */
     @Override
     public User login(UserLoginRequest userLoginRequest) {
+        log.info("----開始登入----");
         User user=userDao.getUserByEmail(userLoginRequest.getEmail());
+
+        //檢查User是否存在
         if (user==null){
             log.warn("此email:{}未被註冊",userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if (user.getPassword().equals(userLoginRequest.getPassword())){
+        //把前端傳來的密碼轉成雜湊直，供下列比較密碼
+        String hashedPassword=DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        //檢查密碼是否正確
+        if (user.getPassword().equals(hashedPassword)){
+            log.info("成功取得user資料");
             return user;
         }else {
             log.warn("此信箱{} 密碼輸入錯誤",userLoginRequest.getEmail());
